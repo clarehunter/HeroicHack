32cc83214066dfdfb855cf97b7969e2d338bafac
clarehunter <clarehunter214@gmail.com>
2018-10-20 20:31:15 -0400

    worked on display cards for comics

--- app/src/main/java/com/clarebhunter/heroichack/ComicsActivity.java
+++ app/src/main/java/com/clarebhunter/heroichack/ComicsActivity.java
@@ -3,15 +3,21 @@ package com.clarebhunter.heroichack;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComicsActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private List<MarvelCharacter> marvelCharacters;
+    private List<Comic> comics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,18 @@ public class ComicsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         marvelCharacters = intent.getParcelableArrayListExtra("charIDs");
-        String names = "";
-        for (MarvelCharacter c: marvelCharacters) {
-            names += c.getName() + "\n";
-        }
-        mTextView = (TextView) findViewById(R.id.textView);
-        mTextView.setText(names);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.comic_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // TODO: fill with correct comics based on characters
+        comics = new ArrayList<>();
+        comics.add(new Comic("SpiderMan", ""));
+        comics.add(new Comic("Captain America", ""));
+
+        mAdapter = new ComicAdapter(comics);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
